2bca13431ca749fa95d6d375d21c2fa741246bfd
nickh881 <nick.huettinger@t-online.de>
2023-04-04 17:29:20 +0200

    Assignment self-experiment (04.04.2023) - updated SimpleList.java to correction - Filter function needs revision (2nd part of assignment)

--- src/main/java/ohm/softa/a04/SimpleList.java
+++ src/main/java/ohm/softa/a04/SimpleList.java
@@ -1,11 +1,20 @@
 package ohm.softa.a04;
 
-public interface SimpleList<T> extends Iterable {
+import java.util.function.Function;
+
+public interface SimpleList<T> extends Iterable<T> {
 	/**
 	 * Add a given object to the back of the list.
 	 */
 	void add(T o);
-	void addDefault();
+	@SuppressWarnings("unchecked")
+	default void addDefault(Class<T> clazz){
+		try{
+			this.add(clazz.newInstance());
+		} catch (InstantiationException | IllegalAccessException e){
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * @return current size of the list
@@ -16,5 +25,35 @@ public interface SimpleList<T> extends Iterable {
 	 * Generate a new list using the given filter instance.
 	 * @return a new, filtered list
 	 */
-	SimpleList<T> filter(SimpleFilter<T> filter);
+	@SuppressWarnings("unchecked")
+	default SimpleList<T> filter(SimpleFilter<T> filter){
+		SimpleList<T> result;
+		try{
+			result = (SimpleList<T>) getClass().newInstance();
+		} catch (InstantiationException | IllegalAccessException e){
+			result = new SimpleListImpl<>();
+		}
+
+		for(T o : this){
+			if(filter.include(o)){
+				result.add(o);
+			}
+		}
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	default <R> SimpleList<R> map(Function<T, R> transform) {
+		SimpleList<R> result;
+		try {
+			result = (SimpleList<R>) this.getClass().newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			result = new SimpleListImpl<>();
+		}
+		for(T t : this){
+			result.add(transform.apply(t));
+		}
+		return result;
+	}
+
 }
